7a01d3dc853f98a114e04101940a23fd6b9aea75
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-01-23 10:37:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-
 buttons.forEach((item) => {
   item.onclick = () => {
     if (item.id == "clear") {
